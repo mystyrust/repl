830ce07cbef72f61579df05009bfeff62d0290c5
mystyrust <mystyrust@gmail.com>
2022-01-24 10:25:39 -0600

    DP app view + home btn

--- phone-arg/phone-mock.js
+++ phone-arg/phone-mock.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
+    $(".home-btn").click((event) => {
+        $(".col").removeClass("app-view")
+    })
+
     $(".app-sc").click((event) => {
         // console.log({ event }, event.currentTarget, $(event.currentTarget))
         // $(this).classList.toggle('.app-view');
         $(".col").removeClass("app-view")
         $(event.currentTarget).toggleClass("app-view")
+        event.preventDefault()
     })
+
+   
+
 })
