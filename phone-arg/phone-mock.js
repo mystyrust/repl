9275668e316b8992ec02bc4a30f6ccfd1969a7c4
mystyrust <mystyrust@gmail.com>
2022-01-27 11:11:43 -0600

    DP - css folder, mock discord and snapchat , status bar

--- phone-arg/phone-mock.js
+++ phone-arg/phone-mock.js
@@ -6,38 +6,36 @@ $(document).ready(function() {
     }
 
     $(".home-btn").click((event) => {
-        $(".app-screen-view div:not(#notification, #notif-backdrop )").css({ height: "0px", width: "0px" , opacity: 0 })
+        $(".app-screen-view > div:not(#notification, #notif-backdrop )")
+            .css({ height: "0px", width: "0px" , opacity: 0, "pointer-events": "none"  })
         closeNotification()
         // var container = $(".app-container")[0]
         // var screen1 = $("#screen1")[0]
         // scrollToElm(container, screen1)
     })
 
-    // $(".phone-crack").click((event) => {
-    //     console.log(event)
-    // })
-
     $(".app-icon").click((event) => {
-        // console.log({ event }, event.currentTarget, $(event.currentTarget))
-        // $(this).classList.toggle('.app-view');
-        // $(".col").removeClass("app-view")
-        // $(event.currentTarget).toggleClass("app-view")
-        // $(".app-screen-view").css({ height: "457px" })
         var appScreen = event.target.attributes['app-target'];
         if (appScreen)
         {
             var appScreenId = appScreen.value;
-            $("#"+appScreenId).css({ height: "457px" , width: "257px", opacity: 1 })
+            $("#"+appScreenId).css({ height: "457px" , width: "257px", opacity: 1, "pointer-events": "visible"  })
             event.preventDefault()
         } else {
             $("#notification").css({ height: "457px" , opacity: 1, "pointer-events": "visible" })
             $("#notif-backdrop").css({ opacity: 0.5 })
             $("#notification").on('click', closeNotification)
-            // setTimeout(closeNotification, 1500)
         }
     
     })
 
-   
+    const setTime = () => {
+        var date = new  Date()
+        var time = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+        $("#time").html(time);
+    }
+    
+    setTime()
 
+    setInterval(setTime, 60*1000)
 })
