de380bb64cc9400b1081a94847466287d1eb364d
mystyrust <mystyrust@gmail.com>
2022-02-03 23:38:01 -0600

    DP fill in a bunch of app mocks

---
+++ dp-arg/scroll.js
@@ -0,0 +1,188 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // i am so sorry for this unholy mess of plain js and jquery, 
+    // i copy / pasted a couple snippets i found online (credited in comments) + added my own edits and....yeah
+    
+    const parentElement = document.getElementsByClassName("app-container")[0] // parent element
+    const childSelector = ".screen-container" // class name that all child scrollables share
+    const pillbugSelector = ".scroller div" // class / selector that all child pillbugs share
+
+    const makeScrollableAndGrabable = (ele, childSelector, pillbugSelector) => { 
+    // drag while on desktop // https://htmldom.dev/drag-to-scroll/
+    ele.style.cursor = 'grab';
+
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+    function isScrolledIntoView(el) {
+        var screen = el.getBoundingClientRect()
+        var parent = ele.getBoundingClientRect()
+        var width = ele.getBoundingClientRect().width
+        
+        var isVisible = Math.abs(screen.left - parent.left) < (width-10)
+        return isVisible;
+    }
+
+    const mouseDownHandler = function (e) {
+        ele.style.cursor = 'grabbing';
+        ele.style.userSelect = 'none';
+
+        pos = {
+            left: ele.scrollLeft,
+            top: ele.scrollTop,
+            // Get the current mouse position
+            x: e.clientX,
+            y: e.clientY,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    const mouseMoveHandler = function (e) {
+        // How far the mouse has been moved
+        const dx = e.clientX - pos.x;
+        const dy = e.clientY - pos.y;
+
+        // Scroll the element
+        ele.scrollTop = pos.top - dy;
+        ele.scrollLeft = pos.left - dx;
+    };
+
+    const mouseUpHandler = function () {
+        ele.style.cursor = 'grab';
+        ele.style.removeProperty('user-select');
+
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    // Attach the handler
+    ele.addEventListener('mousedown', mouseDownHandler);
+
+    // ----------------------------------------//
+
+    // ease scroll within container -- pill bug click // https://stackoverflow.com/a/51005649 method 1
+    $(pillbugSelector).click(event => {
+        var targetId = event.target.attributes['scroll-target'].value;
+        var target = $("#"+targetId)[0]
+        var container = ele
+        scrollToElm(container, target)
+
+        // color the pill bugs accordingly
+    })
+    
+    function scrollToElm(container, elm){
+        var pos = getRelativePos(elm);
+        scrollTo(container, pos.left , 0.4);  // duration in seconds
+    }
+    
+    function getRelativePos(elm){
+      var pPos = elm.parentNode.getBoundingClientRect(), // parent pos
+          cPos = elm.getBoundingClientRect(), // target pos
+          pos = {};
+    
+      pos.top    = cPos.top    - pPos.top,
+      pos.right  = cPos.right  - pPos.right, // + elm.parentNode.scrollLeft,
+      pos.bottom = cPos.bottom - pPos.bottom,
+      pos.left   = cPos.left   - pPos.left + elm.parentNode.scrollLeft;
+    
+      return pos;
+    }
+          
+    function scrollTo(element, to, duration, onDone) {
+        var start = element.scrollLeft,
+            change = to - start,
+            startTime = performance.now(),
+            val, now, elapsed, t;
+    
+        function animateScroll(){
+            now = performance.now();
+            elapsed = (now - startTime)/1000;
+            t = (elapsed/duration);
+    
+            element.scrollLeft = start + change * easeInOutQuad(t);
+    
+            if( t < 1 )
+                window.requestAnimationFrame(animateScroll);
+            else
+                onDone && onDone();
+        };
+    
+        animateScroll();
+    }
+    
+    function easeInOutQuad(t){ return t<.5 ? 2*t*t : -1+(4-2*t)*t };
+
+    $(ele).on('scroll', function(event){
+        $(pillbugSelector).css({ background: 'slategray' })
+        $(childSelector).each((index, screen) => { 
+            if (isScrolledIntoView(screen))
+            {
+                $($(pillbugSelector)[index]).css({ background: 'white' })
+            }
+        })
+
+    });
+
+    }
+
+    makeScrollableAndGrabable(parentElement, childSelector, pillbugSelector)
+    
+    // gallery app
+    makeScrollableAndGrabable($("#gallery-view")[0], ".gallery-item" , ".gallery-scroller div")
+
+    // private gallery behind the fake calculator app 
+    makeScrollableAndGrabable($(".calc-priv")[0], ".privGallery-item" , ".privGallery-scroller div")
+
+//  --------------------img scroller for map?-----------------------------------
+// https://stackoverflow.com/questions/35252249/move-drag-pan-and-zoom-object-image-or-div-in-pure-js
+     
+    var img_ele = null,
+      x_cursor = 0,
+      y_cursor = 0,
+      x_img_ele = 0,
+      y_img_ele = 0;
+    
+    function zoom(zoomincrement) {
+      img_ele = document.getElementById('drag-img');
+      var pre_width = img_ele.getBoundingClientRect().width, pre_height = img_ele.getBoundingClientRect().height;
+      img_ele.style.width = (pre_width * zoomincrement) + 'px';
+      img_ele.style.height = (pre_height * zoomincrement) + 'px';
+      img_ele = null;
+    }
+    
+    document.getElementById('zoomout').addEventListener('click', function() {
+      zoom(0.5);
+    });
+    document.getElementById('zoomin').addEventListener('click', function() {
+      zoom(1.5);
+    });
+    
+    function start_drag() {
+      img_ele = this;
+      x_img_ele = window.event.clientX - document.getElementById('drag-img').offsetLeft;
+      y_img_ele = window.event.clientY - document.getElementById('drag-img').offsetTop;
+      
+    }
+    
+    function stop_drag() {
+      img_ele = null;
+    }
+    
+    function while_drag() {
+      var x_cursor = window.event.clientX;
+      var y_cursor = window.event.clientY;
+      if (img_ele !== null) {
+        img_ele.style.left = (x_cursor - x_img_ele) + 'px';
+        img_ele.style.top = ( window.event.clientY - y_img_ele) + 'px';
+        
+          console.log(img_ele.style.left+' - '+img_ele.style.top);
+    
+      }
+    }
+    
+    document.getElementById('drag-img').addEventListener('mousedown', start_drag);
+    document.getElementsByClassName('mapImg')[0].addEventListener('mousemove', while_drag);
+    document.getElementsByClassName('mapImg')[0].addEventListener('mouseup', stop_drag);
+
+
+});
