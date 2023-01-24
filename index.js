cd59815fa51642453965a755faf8df9f586bc60e
mystyrust <91500391+mystyrust@users.noreply.github.com>
2023-01-24 04:59:58 +0000

    consider: track via ip address instead of cookie

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const fs = require("fs");
-const { createCanvas } = require ("canvas");
+const { createCanvas } = require("canvas");
 
 const app = express();
+
 const ficUrl = "https://archiveofourown.org/works/27314074/chapters/87215485"
 
+app.set('trust proxy', true)
 app.use(require('cookie-parser')());
 
 app.use(function getOrInitId(req, res, next) {
-    var id = req.cookies.id;
-    if(id === undefined || !(id in histories)) {
-        id = Math.random();
-        res.cookie("id", id, { maxAge: 90000000, sameSite: "None", secure: true });
-        histories[id] = [];
-    }
-    req.id = id;
-    next();
+  var id = req.cookies.id;
+  if (id === undefined || !(id in histories)) {
+    id = Math.random();
+    res.cookie("id", id, { maxAge: 90000000, sameSite: "None", secure: true });
+    histories[id] = [];
+  }
+  req.id = id;
+  next();
 })
 
 var histories = {};
@@ -24,18 +26,21 @@ const loops = ["ask-sam", "ask-jazz", "ask-tucker", "ask-elle", "ask-none"]
 app.get("/images/:visited/visited.png", (req, res) => {
   const filePath = "./images/red-x.png"
   const visited = req.params.visited;
+  // console.log("connection", req.connection.remoteAddress);
+  // console.log("socket", req.socket.remoteAddress);
+  // console.log("ip", req.ip);
   // https://www.geeksforgeeks.org/how-to-fetch-images-from-node-server/
 
-  fs.exists(filePath, function (exists) {
-        // Setting default Content-Type
+  fs.exists(filePath, function(exists) {
+    // Setting default Content-Type
     var contentType = "image/png";
-    res.writeHead(200, { 
-            "Content-Type": contentType });
+    res.writeHead(200, {
+      "Content-Type": contentType
+    });
 
-    if (histories[req.id].includes(visited))
-    {
+    if (histories[req.id].includes(visited)) {
       // Reading the file
-      fs.readFile(filePath,function (err, content) {
+      fs.readFile(filePath, function(err, content) {
         res.end(content);
       });
     } else {
@@ -47,23 +52,22 @@ app.get("/images/:visited/visited.png", (req, res) => {
 })
 
 app.get('/next/:visited', (req, res) => {
-    const visited = req.params.visited;
+  const visited = req.params.visited;
 
-    if (!histories[req.id].includes(visited))
-    {
-      histories[req.id].push(visited);
-    }
-    
-   let allFounded = loops.every(l => histories[req.id].includes(l) );
+  if (!histories[req.id].includes(visited)) {
+    histories[req.id].push(visited);
+  }
+
+  let allFounded = loops.every(l => histories[req.id].includes(l));
 
   // handle redirect here
-   if (allFounded) {
+  if (allFounded) {
     res.redirect(ficUrl + "#start2")
-     // res.send(`visited all routes`)
-   } else {
-     // res.send(`havent visited all routes yet! you just visited [${visited}] rn`)
-     res.redirect(ficUrl + "#start")
-   }
+    // res.send(`visited all routes`)
+  } else {
+    // res.send(`havent visited all routes yet! you just visited [${visited}] rn`)
+    res.redirect(ficUrl + "#start")
+  }
 });
 
 app.get('/clear', (req, res) => {
@@ -75,15 +79,15 @@ app.get('/', (req, res) => {
   res.send('yo do we got a hacker here or what')
 });
 
-app.get('/:file', (request, response)=> {
-    var file = request.params.file;
-    response.sendFile(`./html/${file}`, { root: '.' });
+app.get('/:file', (request, response) => {
+  var file = request.params.file;
+  response.sendFile(`./html/${file}`, { root: '.' });
 });
 
-app.get('/:folder/:file', (request, response)=> {
-    var file = request.params.file;
-    var folder = request.params.folder
-    response.sendFile(`./${folder}/${file}`, { root: '.' });
+app.get('/:folder/:file', (request, response) => {
+  var file = request.params.file;
+  var folder = request.params.folder
+  response.sendFile(`./${folder}/${file}`, { root: '.' });
 });
 
 app.listen(3000, () => {
